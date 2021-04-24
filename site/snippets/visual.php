71ac6de80ad920866131cff549fc6b39622c0358
Tiana Dueck <tianadueck@pm.me>
2021-04-23 23:17:42 -0400

    site/snippets/visual.php - omg it works (i think it was changing $image to $file in the caption)

--- site/snippets/visual.php
+++ site/snippets/visual.php
@@ -1,46 +1,31 @@
 <div id="threeBlocks">
 
-  <?php foreach ($page->images() as $image) : ?>
+  <?php foreach ($page->files()->sortBy('sort', 'asc') as $file) : ?>
 
-    <div class="threeBlock">
+    <?php if ($file->type() == 'image') : ?>
+      <div class="threeBlock">
+        <figure class="threeBox">
+          <img style="width: 200px;" src="<?= $file->url() ?>" alt="<?= $page->title()->html() ?>" />
+          <figcaption><?= $file->caption() ?></figcaption>
+        </figure>
+      </div>
+    <?php endif ?>
 
-      <figure class="threeBox">
-
-        <img src="<?= $image->url() ?>" alt="<?= $image->alt() ?>">
-
-        <figcaption><?= $image->caption() ?></figcaption>
-
-      </figure>
-
-    </div>
+    <?php if ($file->type() == 'video') : ?>
+      <div class="threeBlock">
+        <figure class="threeBox">
+          <video style="width: 200px;" controls>
+            <source src="<?= $file->url() ?>" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          <figcaption><?= $file->caption() ?></figcaption>
+        </figure>
+      </div>
+    <?php endif ?>
 
   <?php endforeach ?>
 
 
 </div>
 
-
-<?php foreach ($page->files() as $file) : ?>
-
-  <?php if ($file->type() == 'image') : ?>
-    <div class="threeBlock">
-      <figure class="threeBox">
-        <img style="width: 200px;" src="<?= $file->url() ?>" alt="<?= $page->title()->html() ?>" />
-        <figcaption><?= $image->caption() ?></figcaption>
-      </figure>
-    </div>
-  <?php endif ?>
-
-  <?php if ($file->type() == 'video') : ?>
-    <div class="threeBlock">
-      <figure class="threeBox">
-        <video style="width: 200px;" controls>
-          <source src="<?= $file->url() ?>" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <figcaption><?= $file->caption() ?></figcaption>
-      </figure>
-    </div>
-  <?php endif ?>
-
-<?php endforeach ?>
+<!-- um... wow -->
