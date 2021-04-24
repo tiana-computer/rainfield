fc83d175ad4cc688f42d767d3d24973521352082
Tiana Dueck <tianadueck@pm.me>
2021-04-23 23:04:01 -0400

    snippets/visual.php - if statement working until the threeBlocks div wraps..

--- site/snippets/visual.php
+++ site/snippets/visual.php
@@ -19,22 +19,28 @@
 
 </div>
 
+
 <?php foreach ($page->files() as $file) : ?>
 
   <?php if ($file->type() == 'image') : ?>
-    <figure>
-      <img style="width: 200px;" src="<?= $file->url() ?>" alt="<?= $page->title()->html() ?>" />
-    </figure>
+    <div class="threeBlock">
+      <figure class="threeBox">
+        <img style="width: 200px;" src="<?= $file->url() ?>" alt="<?= $page->title()->html() ?>" />
+        <figcaption><?= $image->caption() ?></figcaption>
+      </figure>
+    </div>
   <?php endif ?>
 
   <?php if ($file->type() == 'video') : ?>
-    <figure>
-      <video style="width: 200px;" controls>
-        <source src="<?= $file->url() ?>" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-    </figure>
-
+    <div class="threeBlock">
+      <figure class="threeBox">
+        <video style="width: 200px;" controls>
+          <source src="<?= $file->url() ?>" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <figcaption><?= $file->caption() ?></figcaption>
+      </figure>
+    </div>
   <?php endif ?>
 
 <?php endforeach ?>
