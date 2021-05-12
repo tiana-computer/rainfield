e96ec36740a498096c0ecfd377cbcf43f4de3878
Tiana Dueck <tianadueck@pm.me>
2021-05-12 15:10:51 -0400

    modal images

--- site/snippets/nav.php
+++ site/snippets/nav.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-  <link rel="shortcut icon" type="image/png" href="assets/drop.png" >
+  <link rel="icon" type="image/png" href="assets/drop.png" >
 
 
   <title><?= $site->title() ?></title>
