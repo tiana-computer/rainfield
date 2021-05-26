9e791939d4f188f061c8e1e8a9d2b0855c4ac5e6
Tiana Dueck <tianadueck@pm.me>
2021-05-25 20:50:00 -0400

    fav

--- site/snippets/nav.php
+++ site/snippets/nav.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-  <link rel="icon" type="image/png" href="assets/drop.png" >
+  <link rel="icon" type="image/png" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>r</text></svg>" >
 
 
   <title><?= $site->title() ?></title>
