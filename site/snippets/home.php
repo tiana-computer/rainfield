1ed49a1d654e22cf197201f12c5ac2f94bba9702
Tiana Dueck <tianadueck@pm.me>
2021-08-01 12:39:22 -0400

    style captions

--- site/snippets/home.php
+++ site/snippets/home.php
@@ -5,7 +5,7 @@
 
         <figure>
           <img style="width: 500px;" src="<?= $file->url() ?>" alt="<?= $page->title()->html() ?>" />
-          <figcaption id="caption"><?= $file->caption() ?></figcaption>
+          <figcaption class="captionHome"><?= $file->caption() ?></figcaption>
         </figure>
 
     <?php endif ?>
@@ -17,7 +17,7 @@
             <source src="<?= $file->url() ?>#t=1" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
-          <figcaption><?= $file->caption() ?></figcaption>
+          <figcaption class="captionHome"><?= $file->caption() ?></figcaption>
         </figure>
 
     <?php endif ?>
