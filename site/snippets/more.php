fcbf81abe036cdfb482ce57fc42a47d84f7be3b1
Tiana Dueck <tianadueck@pm.me>
2021-08-04 17:45:59 -0400

    MORE PAGE LINKS

--- site/snippets/more.php
+++ site/snippets/more.php
@@ -19,14 +19,3 @@
 <?php endforeach ?>
 
 </div>
-
-<!--
-<div id="blocks">
-    <?php foreach ($page->children() as $more) : ?>
-      <a href="<?= $more->input() ?>" target="_blank">
-        <div class="block">
-          <p class="box"><?= $more->title() ?></p>
-        </div>
-      </a>
-    <?php endforeach ?>
-  </div>-->
