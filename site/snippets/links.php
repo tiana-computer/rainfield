5b517db3a0f50c7f3adaf790acccb1c615cd353f
Tiana Dueck <tianadueck@pm.me>
2021-08-04 18:11:06 -0400

    class link h2

--- site/snippets/links.php
+++ site/snippets/links.php
@@ -12,7 +12,7 @@
 <?php foreach ($page->children() as $links) : ?>
 
   <div class="textOneBlock" style="max-width: 70%;">
-    <h2><a class="link" href="<?= $links->title() ?>"><?= $links->title() ?><a></h2>
+    <h2 class="link"><a href="<?= $links->title() ?>"><?= $links->title() ?><a></h2>
     <p><?= $links->input() ?></p>
   </div>
 
