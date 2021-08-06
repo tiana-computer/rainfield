ff6efc549ca186145edcc46c22bbc7020c845097
Tiana Dueck <tianadueck@pm.me>
2021-08-06 12:49:06 -0400

    input yes link

--- site/snippets/links.php
+++ site/snippets/links.php
@@ -12,7 +12,7 @@
 <?php foreach ($page->children() as $links) : ?>
 
   <div class="textOneBlock" style="max-width: 100%;">
-    <h2 class="link"><a href="<?= $links->title() ?>"><?= $links->title() ?><a></h2>
+    <h2 class="link"><a href="<?= $links->input() ?>"><?= $links->title() ?><a></h2>
   </div>
 
 <?php endforeach ?>
