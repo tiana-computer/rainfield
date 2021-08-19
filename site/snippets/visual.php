0986003059aecc401e44ecbafb0545f659ef1e01
Tiana Dueck <tianadueck@pm.me>
2021-08-18 20:54:10 -0400

    visual hear me out

--- site/snippets/visual.php
+++ site/snippets/visual.php
@@ -220,7 +220,7 @@ img.hover-shadow {
     <?php if ($file->type() == 'image') : ?>
       <div class="threeBlock">
         <figure class="threeBox">
-          <img class="cursor visualGal" style="width:100%" onclick="openModal();currentSlide($value)" src="<?= $file->url() ?>" alt="<?= $page->title()->html() ?>" />
+          <img class="cursor visualGal" style="width:100%" onclick="openModal();currentSlide(4)" src="<?= $file->url() ?>" alt="<?= $page->title()->html() ?>" />
           <figcaption><?= $file->caption() ?></figcaption>
         </figure>
       </div>
