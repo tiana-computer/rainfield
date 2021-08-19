3c657870beab36bd767e30d00000d02189db827a
Tiana Dueck <tianadueck@pm.me>
2021-08-18 20:42:57 -0400

    visual &#8620;

--- site/snippets/visual.php
+++ site/snippets/visual.php
@@ -256,8 +256,8 @@ img.hover-shadow {
     <?php endforeach ?>
 
 
-    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    <a class="prev" onclick="plusSlides(-1)">&#8619;</a>
+    <a class="next" onclick="plusSlides(1)">&#8620;</a>
 
     <div class="caption-container">
       <p id="caption"></p>
