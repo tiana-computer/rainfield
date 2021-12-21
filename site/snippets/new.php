f94ef4ad35b5fef4b38dad2cdfe7867c3606817e
Tiana Dueck <tianadueck@pm.me>
2021-12-21 18:24:11 -0500

    neew

--- site/snippets/new.php
+++ site/snippets/new.php
@@ -12,7 +12,7 @@
 <?php foreach ($page->children() as $new) : ?>
 
   <div class="textOneBlock" style="max-width: 100%;">
-    <h2 class="link"><a href="<?= $new->input() ?>"><?= $new->title() ?><a></h2>
+    <h2 class="link"><a href="<?= $link->input() ?>"><?= $link->title() ?><a></h2>
   </div>
 
 <?php endforeach ?>
