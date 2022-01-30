ba2913e5d9c6120cae4702df95ed5b82c263d983
Tiana Dueck <tianadueck@pm.me>
2022-01-30 17:35:38 -0500

    home

--- site/snippets/home.php
+++ site/snippets/home.php
@@ -1,47 +1,46 @@
 <style>
-
-* {
+  * {
     scrollbar-color: white blue !important;
     scrollbar-width: thin !important;
   }
 
-.captionHome {
+  .captionHome {
     color: grey;
     font-size: 10pt;
     text-align: center;
     padding: 10px 0 0;
     margin: 0;
-}
+  }
 
-.home-container {
-  grid-template-columns: 1fr 1fr ;
-}
-
+  .home-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
 </style>
 
 <div class="home-container">
-  
-<div class="home-item">
 
-<p><?= $page->about() ?></p>
+  <div class="home-item">
 
-</div>
+    <p><?= $page->about() ?></p>
 
-<div class="home-item">
+  </div>
 
-<?php foreach ($page->files()->sortBy('sort', 'asc') as $file) : ?>
+  <div class="home-item">
 
-    <?php if ($file->type() == 'image') : ?>
+    <?php foreach ($page->files()->sortBy('sort', 'asc') as $file) : ?>
+
+      <?php if ($file->type() == 'image') : ?>
 
         <figure>
           <img style="display: block; margin: 0 auto; width: 40vw; max-with: 400px;" src="<?= $file->url() ?>" alt="<?= $page->title()->html() ?>" />
           <figcaption class="captionHome"><?= $file->caption() ?></figcaption>
         </figure>
 
-    <?php endif ?>
+      <?php endif ?>
 
-    <?php if ($file->type() == 'video') : ?>
-  
+      <?php if ($file->type() == 'video') : ?>
+
         <figure class="threeBox">
           <video style="display: block; margin: 0 auto; width: 40vw; max-with: 400px;" controls>
             <source src="<?= $file->url() ?>#t=1" type="video/mp4" />
@@ -50,11 +49,10 @@
           <figcaption class="captionHome"><?= $file->caption() ?></figcaption>
         </figure>
 
-    <?php endif ?>
+      <?php endif ?>
 
-  <?php endforeach ?>
+    <?php endforeach ?>
 
-    </div>
+  </div>
 
-    </div>
-
+</div>
