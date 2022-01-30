979acb774073d51f90ae5755663b584830a8c848
Tiana Dueck <tianadueck@pm.me>
2022-01-30 17:36:33 -0500

    height width home img

--- site/snippets/home.php
+++ site/snippets/home.php
@@ -33,7 +33,7 @@
       <?php if ($file->type() == 'image') : ?>
 
         <figure>
-          <img style="display: block; margin: 0 auto; width: 40vw; max-with: 400px;" src="<?= $file->url() ?>" alt="<?= $page->title()->html() ?>" />
+          <img style="display: block; margin: 0 auto; height: 70vw; width: auto;" src="<?= $file->url() ?>" alt="<?= $page->title()->html() ?>" />
           <figcaption class="captionHome"><?= $file->caption() ?></figcaption>
         </figure>
 
@@ -42,7 +42,7 @@
       <?php if ($file->type() == 'video') : ?>
 
         <figure class="threeBox">
-          <video style="display: block; margin: 0 auto; width: 40vw; max-with: 400px;" controls>
+          <video style="display: block; margin: 0 auto; height: 70vw; width: auto;" controls>
             <source src="<?= $file->url() ?>#t=1" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
