658071b0b24422e9da5b5253e91a0e9a76938afa
Tiana Dueck <tianadueck@pm.me>
2022-01-30 18:08:21 -0500

    width fig home

--- site/snippets/home.php
+++ site/snippets/home.php
@@ -60,7 +60,7 @@ a:hover {
       <?php if ($file->type() == 'image') : ?>
 
         <figure style="border: 1px solid blue; width: 100%;">
-          <img style="display: block; margin: 0 auto; height: 70vh;" src="<?= $file->url() ?>" alt="<?= $page->title()->html() ?>" />
+          <img style="display: block; margin: 0 auto; height: 70vh; width: 100%;" src="<?= $file->url() ?>" alt="<?= $page->title()->html() ?>" />
           <figcaption class="captionHome"><?= $file->caption() ?></figcaption>
         </figure>
 
